da988848df787b61815db5ad3c62809d0e16eea4
Ujjwa <ujjwalkumarmsg@gmail.com>
2024-06-09 18:30:13 +0530

    05062024

---
+++ EX_06062024/lab010.py
@@ -0,0 +1,28 @@
+# variable name should be start grom A-Z, a-z _
+name = "Ujjwal"
+print(type(name))
+
+# variable name should not start with number
+# 123 =345 - variable names
+
+
+# variable name should not be any keyword
+# keyword ? // Reserved word
+# 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield' [2]
+# and = 123
+
+# variable name should not be any special character
+# @ = 123
+# $ = 344
+_ = 123
+_name = "ujjwal"
+# $pramod = "ujjwal"
+# pramod$ = "Hello"
+
+# variable name should not be any space
+# first name = "Ujjwal"
+first_name = "Ujjwal"
+# Python love the snake case
+variable_name = "ujjwal"
+
+long_var_name_is_created_here = "Hello"
